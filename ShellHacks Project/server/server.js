fed7a8535e63b7aa3e6608e9608f84a621af8071
Curt <Curtf0505@gmail.com>
2024-09-29 06:51:31 -0400

    env changes

--- ShellHacks Project/server/server.js
+++ ShellHacks Project/server/server.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
-require('dotenv').config();
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
